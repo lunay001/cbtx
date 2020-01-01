a62214e17acf48063a3080132288e4eda0fe9a5c
mingyue <lunay@mingyuedeMacBook-Pro.local>
2020-01-01 20:57:52 +0800

    优化代码

--- src/lib/hope_sql.erl
+++ src/lib/hope_sql.erl
@@ -8,17 +8,39 @@
 %%%-------------------------------------------------------------------
 -module(hope_sql).
 -author("lunay").
+-include("common.hrl").
 
 %% API
--export([query/1, fetch_row/1, fetch_rows/1]).
+-export([fetch_row/1, fetch_rows/0, fetch_one2/1, query/1]).
 
 query(Sql) ->
 	mysql_poolboy:query(pool, Sql).
 
-fetch_row(_id) ->
-	mysql_poolboy:query(pool, "SELECT * FROM `t1` WHERE id = ? LIMIT 1", [_id]).
+%% API
+fetch_row(Id) ->
+	{ok, FieldList, DataList} = mysql_poolboy:query(?DB_MYSQL_POOL, "SELECT * FROM foo where id = ? LIMIT 1", [Id]),
+	if
+		length(DataList) > 0 ->
+			[H|_] = DataList,
+			Data = lists:zip(FieldList, H);
+		true ->
+			Data = #{}
+	end,
+	{ok,  Data}.
 
-fetch_rows(N) ->
-	mysql_poolboy:query(pool, "SELECT * FROM `t1` WHERE 1 LIMIT ?", [N]).
+fetch_rows() ->
+	{ok, FieldList, UserList} = mysql_poolboy:query(?DB_MYSQL_POOL, "SELECT * FROM foo where 1 LIMIT 100", []),
+	DataList = [lists:zip(FieldList, User) || User <- UserList],
+	{ok,  DataList}.
 
+fetch_one2(Id) ->
+	{ok, FieldList, DataList} = mysql_poolboy:query(?DB_MYSQL_POOL, "SELECT * FROM foo where id = ? LIMIT 1", [Id]),
+	case length(DataList) > 0 of
+		true ->
+			[H|_] = DataList,
+			Data = lists:zip(FieldList, H);
+		false ->
+			Data = #{}
+	end,
+	{ok, Data}.
 
